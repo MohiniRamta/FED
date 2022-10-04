d9e8f08f3b95b74987bbaa4d8e98949082691eb3
MohiniRamta <112831583+MohiniRamta@users.noreply.github.com>
2022-10-04 17:49:58 +0200

    Laatste update 04/10/2022

--- scripts/script.js
+++ scripts/script.js
@@ -1,12 +1,27 @@
-// JavaScript Document
 console.log("hi");
 
-var menuKnop = document.querySelector("header nav button");
 
-menuKnop.addEventListener("click", toggleMenu);
+var talenKnop = document.querySelector("body header section button")
+var testKnop = document.querySelector("header > nav button");
+var witteBalk = document.getElementById("mops")
 
-function toggleMenu() {
-	var deNav = document.querySelector("header nav button");
-	deNav.classList.toggle("open");
+
+
+talenKnop.addEventListener("click", toggleTalen);
+
+function toggleTalen() {
+	var deTalen = document.querySelector("section ul");
+	deTalen.classList.toggle("open");
 }
 
+
+function toggleMenu()  {
+	var deNav = document.querySelector("header nav ul");
+	deNav.classList.toggle("open");
+	
+} testKnop.addEventListener("click", toggleMenu);
+
+
+function balkSchuif() {
+	witteBalk.classList.toggle("schuif");
+} testKnop.addEventListener("click", balkSchuif);
